2caba025e28e1be4bbff7346ca99c8356a583fa4
Rafael George <george.rafael@gmail.com>
2013-07-23 14:34:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoaasRb::Application.config.secret_token = '766fd595e10fc57e93e8093f88aea0725276ccd701c697fa1c10f4c23777ca169c428315696045abb20bd1730233965702d7941a2d2a2cc6e336449f90990e2c'
